7692d3bfb6a28310e5411fc046e5da25fcb535dd
Gowiem <gowie.matt@gmail.com>
2014-07-03 19:55:51 -0400

    Updates functions

---
+++ functions/gowie_functions.zsh
@@ -0,0 +1,18 @@
+# iTerm Tab Coloring, Fuck Ya!
+function tab_red() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'red'; }
+function tab_yellow() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'yellow'; }
+function tab_blue() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'blue'; }
+function tab_green() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'green'; }
+
+function chpwd() { ~/Workspace/Colorize/colorize_tabs.rb -d }
+
+# Easily get IP addresses
+function external_ip () { curl http://ipecho.net/plain; echo; }
+
+# History of CG Django Helpers 
+
+function haml_all_the_things() {
+  find . -name '*erb' | \
+  xargs ruby -e 'ARGV.each { |i| puts "html2haml -r #{i} #{i.sub(/erb$/,"haml")}"}' | \
+  bash;
+}
