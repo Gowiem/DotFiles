77e5e764fd6feca6c2f1f32d70b97841dec9ee5a
hasMattG <mattg@tune.com>
2015-07-28 13:45:52 -0400

    Removes colorize funcs. I'm using regular terminal now.

--- functions/gowie_functions.zsh
+++ functions/gowie_functions.zsh
@@ -1,10 +1,10 @@
 # iTerm Tab Coloring, Fuck Ya!
-function tab_red() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'red'; }
-function tab_yellow() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'yellow'; }
-function tab_blue() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'blue'; }
-function tab_green() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'green'; }
+# function tab_red() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'red'; }
+# function tab_yellow() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'yellow'; }
+# function tab_blue() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'blue'; }
+# function tab_green() { /Users/gowie/Workspace/Colorize/colorize_tabs.rb -c 'green'; }
 
-function chpwd() { ~/Workspace/Colorize/colorize_tabs.rb -d }
+# function chpwd() { ~/Workspace/Colorize/colorize_tabs.rb -d }
 
 # Easily get IP addresses
 function external_ip () { curl http://ipecho.net/plain; echo; }
