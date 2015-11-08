19e3a31d66fe6045d51ade791992ea1046548203
Matt Gowie <mattg@tune.com>
2015-11-08 15:31:43 -0800

    Adds refresh_network command

--- functions/gowie_functions.zsh
+++ functions/gowie_functions.zsh
@@ -16,3 +16,11 @@ function haml_all_the_things() {
   xargs ruby -e 'ARGV.each { |i| puts "html2haml -r #{i} #{i.sub(/erb$/,"haml")}"}' | \
   bash;
 }
+
+# Refresh the network when it starts messing up on Mac
+
+function refresh_network() {
+  sudo ifconfig en1 down;
+  sudo route flush;
+  sudo ifconfig en1 up;
+}
