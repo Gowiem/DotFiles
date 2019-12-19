8916d64c304b53dd3f2747d232a203d79726bc4b
Matt Gowie <matt@masterpoint.io>
2019-12-18 22:47:38 -0500

    Updates sourcing virtualenvwrapper with check

--- python/path.zsh
+++ python/path.zsh
@@ -3,4 +3,7 @@ export PATH="/Library/Python/2.7/site-packages:$PATH"
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Workspace
 
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]
+then
 source /usr/local/bin/virtualenvwrapper.sh
+fi
