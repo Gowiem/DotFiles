dc9337b540d297d3306481346ff4c3e12f2ec2fc
Matt Gowie <matt@masterpoint.io>
2020-04-23 13:54:09 -0600

    Updates AWS setup
    No longer doing individual aws-exec commands. Switched to loading 
    profile to shell and then I don't run into function and alias issues.

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -3,9 +3,6 @@ alias aws_creds="atom ~/.aws/credentials"
 # AWS Vault
 alias av='aws-vault'
 
-alias avnk='aws-vault exec nk-terraform --'
-alias avgow='aws-vault exec gowiem --'
-alias avsubgow='aws-vault exec sub-gowiem --'
-
+# SSM start-sessions
 alias gow_sesh='avgow aws ssm start-session --target'
 alias nk_sesh='avnk aws ssm start-session --target'
--- aws/functions.zsh
+++ aws/functions.zsh
@@ -1,6 +1,24 @@
 function aws_reset_session() {
-  for var in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+  for var in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN VAULT_PROFILE AWS_VAULT AWS_DEFAULT_REGION AWS_REGION AWS_SECURITY_TOKEN AWS_SESSION_EXPIRATION
   do
     unset ${var}
   done
+}
+
+function aws_print_creds() {
+  for var in "AWS_VAULT" "AWS_ACCESS_KEY_ID" "AWS_SESSION_TOKEN"; do
+    echo "$var: ${(P)var}"
+  done
+}
+
+function avprof() {
+  # Set VAULT_PROFILE which is used in my Right Prompt
+  export VAULT_PROFILE=$1
+
+  # Exec the given profile and include the new AWS Creds into the current shell session
+  source <(aws-vault exec $VAULT_PROFILE --no-session -- sh -c 'export -p')
+}
+
+function aws_ecr_login() {
+  eval $(aws ecr get-login --region us-east-1 --no-include-email)
 }
