5585ae623c4ea731c38c8163a2f0005f26291347
Matt Gowie <matt@masterpoint.io>
2020-05-14 13:36:41 -0600

    Adds pgp/init for pgp setup

---
+++ pgp/init.zsh
@@ -0,0 +1,5 @@
+export GPG_TTY=$(tty)
+
+# WTF https://github.com/Homebrew/homebrew-core/issues/14737
+# brew install pinentry-mac
+# echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
