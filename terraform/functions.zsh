674d124b0057a202942839c22104b9095a7a7283
Matt Gowie <matt@masterpoint.io>
2020-06-03 17:07:44 -0600

    Adds tf_make_module function

--- terraform/functions.zsh
+++ terraform/functions.zsh
@@ -1,7 +1,7 @@
 function tfplan() {
   if [[ -d "./.terraform/" ]]; then
 
-    workspace_name=$( terraform workspace show )
+    workspace_name=$(terraform workspace show)
     if [[ -f "tfvars/${workspace_name}.tfvars" ]]; then
       echo "Running terraform plan with ${workspace_name}.tfvars..."
       WORKSPACE_ARG="-var-file=tfvars/${workspace_name}.tfvars"
@@ -16,4 +16,19 @@ function tfplan() {
   else
     echo "You're not in a terraform directory!"
   fi
+}
+
+function tf_make_module() {
+  local module_name=$1
+
+  if [[ ! -d "./.terraform/" ]]; then
+    echo "You're not in a terraform directory!"
+    exit 1
+  fi
+
+  mkdir -p "./modules/${module_name}"
+  echo "# ${module_name}.tf\n" >"./modules/${module_name}/main.tf"
+  echo "# ${module_name}.tf\n" >"./modules/${module_name}/variables.tf"
+  echo "# ${module_name}.tf\n" >"./modules/${module_name}/outputs.tf"
+  echo "Successfully created ${module_name} module!"
 }
