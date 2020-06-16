37716b1024dbd89a29508c1712e839c02b07687e
Matt Gowie <matt@masterpoint.io>
2020-06-16 14:49:14 -0600

    Changes over KUBE_EDITOR to vscode > vim.
    God do I hate vim.

--- system/env.zsh
+++ system/env.zsh
@@ -1,8 +1,8 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
-if [[ "$EDITOR" == "" ]] ; then
+if [[ "$EDITOR" == "" ]]; then
     # Use VSCode for my editor.
     export EDITOR='code --wait'
-    export KUBE_EDITOR="vim"
+    export KUBE_EDITOR="code --wait"
 fi
 
 export PROJECTS=~/Workspace
