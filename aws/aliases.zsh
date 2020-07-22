16117c75d7afea07fc53f616432b6f837fc2c5c6
Matt Gowie <matt@masterpoint.io>
2020-07-22 13:53:21 -0600

    Adds avl alias for aws-vault login

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -4,6 +4,7 @@ alias aws_info="aws sts get-caller-identity"
 
 # AWS Vault
 alias av='aws-vault'
+alias avl='aws-vault login'
 
 # SSM start-sessions
 alias gow_sesh='avgow aws ssm start-session --target'
