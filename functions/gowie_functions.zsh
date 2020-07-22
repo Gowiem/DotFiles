7a780cb5c7c732b3eb4cbb7445ff99f0af0b7dd5
Matt Gowie <matt@masterpoint.io>
2020-07-22 14:30:55 -0600

    Adds stree function for opening source tree

--- functions/gowie_functions.zsh
+++ functions/gowie_functions.zsh
@@ -1,16 +1,23 @@
 # Easily get IP addresses
-function external_ip () { curl http://ipecho.net/plain; echo; }
+function external_ip() {
+  curl http://ipecho.net/plain
+  echo
+}
 
 # History of CG Django Helpers
 function haml_all_the_things() {
-  find . -name '*erb' | \
-  xargs ruby -e 'ARGV.each { |i| puts "html2haml -r #{i} #{i.sub(/erb$/,"haml")}"}' | \
-  bash;
+  find . -name '*erb' |
+    xargs ruby -e 'ARGV.each { |i| puts "html2haml -r #{i} #{i.sub(/erb$/,"haml")}"}' |
+    bash
 }
 
 # Refresh the network when it starts messing up on Mac
 function refresh_network() {
-  sudo ifconfig en1 down;
-  sudo route flush;
-  sudo ifconfig en1 up;
+  sudo ifconfig en1 down
+  sudo route flush
+  sudo ifconfig en1 up
+}
+
+function stree() {
+  open /Applications/SourceTree.app
 }
