562e090b0925535817cc50201f117abe866a2be1
Matt Gowie <matt@masterpoint.io>
2020-10-06 10:46:47 -0600

    Fixes GOPATH + sourcing

--- go/env.zsh
+++ go/env.zsh
@@ -1,2 +1,4 @@
 export GOPATH=~/Workspace/go
+export GOBIN=~/Workspace/go/bin
 export GOEXE=/usr/local/bin/go
+export PATH=$PATH:$GOBIN
--- go/path.zsh
+++
@@ -1,1 +0,0 @@
-test -s "/usr/local/bin/go" && export PATH=$PATH:$(go env GOPATH)/bin
