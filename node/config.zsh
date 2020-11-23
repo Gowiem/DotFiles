6f69ff157cb298e20a3a153846d373f4dba2bbee
Matt Gowie <matt@masterpoint.io>
2020-11-22 19:30:21 -0700

    feat: adds nvm wrapper script

--- node/config.zsh
+++
@@ -1,4 +0,0 @@
-# This loads NVM if it's around.
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
