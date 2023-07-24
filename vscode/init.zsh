185eb4c1a6d88de1b1e3e244ab915511ec77dcb1
Matt Gowie <matt@masterpoint.io>
2023-07-24 14:47:59 -0600

    chore: fixing up issues with recurring OSX checks

--- vscode/init.zsh
+++ vscode/init.zsh
@@ -1,4 +1,4 @@
 # Add Visual Studio Code (code) to PATH
-if [ "$(uname -s)" == "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
   export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 fi
