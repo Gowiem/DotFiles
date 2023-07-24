3cd187b08caf20cde08c7c1927ca7c72b64064fb
Matt Gowie <matt@masterpoint.io>
2023-07-24 14:43:00 -0600

    chore: fixes from using on linux

--- node/env.zsh
+++ node/env.zsh
@@ -1,3 +1,3 @@
-eval "$(fnm env --use-on-cd)"
+command -v fnm &> /dev/null && eval "$(fnm env --use-on-cd)"
 
-ln -sf $(which node) ~/.local/bin/node
+command -v node &> /dev/null && ln -sf $(which node) ~/.local/bin/node
